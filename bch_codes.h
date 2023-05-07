3df39aced06cdf19ac1844e12721ef68bd926489
diS3e <geli1610@yandex.ru>
2023-05-07 23:35:42 +0300

    fix: fix coding feat: add hard decoding with berlekamp_massey algorithm

--- bch_codes.h
+++ bch_codes.h
@@ -10,20 +10,19 @@
 #include <bitset>
 
 
-struct galua_field {
-    std::map<int, int> log_to_element;
-    std::map<int, int> element_to_log;
+struct galois_field {
+    int *log_to_element;
+    int *element_to_log;
     int m;
-    int GF_power;
 
-    explicit galua_field(int generating_polynomial) {
+    explicit galois_field(int generating_polynomial) {
         int prev = 1;
         m = (int) log2(generating_polynomial);
-        GF_power = (1 << m);
+        log_to_element = new int[(1 << m) - 1];
+        element_to_log = new int[(1 << m) - 1];
         for (int i = 0; i < (1 << m) - 1; ++i) {
-            std::cout << "alpha^" << i << ": " << std::bitset<5>(prev) << "\n";
-            log_to_element.insert({i, prev});
-            element_to_log.insert({prev, i});
+            log_to_element[i] = prev;
+            element_to_log[prev] = i;
             prev <<= 1;
             if (prev >= (1 << m)) {
                 prev = prev ^ generating_polynomial;
@@ -31,60 +30,118 @@ struct galua_field {
         }
     }
 
-    void check_containing_element(int a) {
+//    galois_field(galois_field const &GF) {
+//        log_to_element = new int[(1 << GF.m) - 1];
+//        element_to_log = new int[(1 << GF.m) - 1];
+//        memcpy(log_to_element, GF.log_to_element, (1 << GF.m) - 1);
+//        memcpy(element_to_log, GF.element_to_log, (1 << GF.m) - 1);
+//        m = GF.m;
+//    }
+
+//    galois_field& operator=(const galois_field &other) {
+//        using std::swap;
+//        if (&other != this) {
+//            galois_field GF(other);
+//            swap(log_to_element, GF.log_to_element);
+//            swap(element_to_log, GF.element_to_log);
+//            swap(m, GF.m);
+//        }
+//        return *this;
+//    }
+
+    void check_containing_element(int a) const {
         if (!(0 <= a && a < (1 << m))) {
-            throw "Element not exists in this field";
+            throw std::range_error("Element not exists in this field");
         }
     }
 
 //Складывает элементы поля
-    int sum_elements(int a, int b) {
+    [[nodiscard]] int sum_elements(int a, int b) const {
         check_containing_element(a);
         check_containing_element(b);
         return a ^ b;
     }
 
 //Умножает элементы поля
-    int multiply_elements(int a, int b) {
+    [[nodiscard]] int multiply_elements(int a, int b) const {
         check_containing_element(a);
         check_containing_element(b);
         if (a == 0 || b == 0) {
             return 0;
         }
-        return log_to_element[(element_to_log[a] + element_to_log[b]) % (GF_power - 1)];
+        return log_to_element[(element_to_log[a] + element_to_log[b]) % ((1 << m) - 1)];
     }
+
 //Возвращает логарифм элемента
-    int getLog(int element) {
+    [[nodiscard]] int getLog(int element) const {
         check_containing_element(element);
         if (element == 0) {
-            throw "Can't get log from 0";
+            throw std::range_error("Can't get log from 0");
         }
         return element_to_log[element];
     }
+
 //Возвращает элемент по логарифму
-    int getElement(int log) {
-        log = ((log % (GF_power - 1)) + (GF_power - 1)) % (GF_power - 1);
+    [[nodiscard]] int getElement(int log) const {
+        log = ((log % ((1 << m) - 1)) + ((1 << m) - 1)) % ((1 << m) - 1);
         return log_to_element[log];
+    }
+
+    [[nodiscard]] int getInverse(int a) const {
+        return getElement(-getLog(a));
+    }
+
+    [[nodiscard]] int getPower(int base, int pow) const {
+        if (base == 0) {
+            if (pow == 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            return getElement(getLog(base) * pow);
+        }
     }
 };
 
-int get(int i, const std::vector<int>& v){
-    return (i < v.size()) ? v[i] : 0;
+//Многочлен над конечным полем: vector где степени записаны по возрастанию a[i] - коэффициент при x^i
+// Все операции возвращают массивы, с потенциально нулевыми старшими коэффициентами.
+
+
+int get(int i, const std::vector<int> &v) {
+    return (0 <= i && i < v.size()) ? v[i] : 0;
 }
 
-std::vector<int> multiply_polynomial(galua_field GF, const std::vector<int> &P, const std::vector<int> &Q) {
+// Метод shrink - убирается нулевые старшие степени
+std::vector<int> shrink(const std::vector<int> &a) {
+    std::vector<int> result(a);
+    while (!result.empty() && result[result.size() - 1] == 0) {
+        result.erase(result.end() - 1);
+    }
+    return result;
+}
+
+std::vector<int> multiply_polynomial(const galois_field &GF, const std::vector<int> &P, const std::vector<int> &Q) {
     std::vector<int> ans(P.size() + Q.size() - 1);
     for (int i = 0; i < ans.size(); ++i) {
         int temp = 0;
         for (int j = 0; j <= i; ++j) {
-            temp = GF.sum_elements(temp, GF.multiply_elements(get(j, P), get(i-j, Q)));
+            temp = GF.sum_elements(temp, GF.multiply_elements(get(j, P), get(i - j, Q)));
         }
         ans[i] = temp;
     }
     return ans;
 }
 
-int get_generating_polynomial(int n, int k, int delta) {
+std::vector<int> summing_polynomial(const galois_field &GF, const std::vector<int> &P, const std::vector<int> &Q) {
+    std::vector<int> ans(std::max(P.size(), Q.size()));
+    for (int i = 0; i < ans.size(); ++i) {
+        ans[i] = GF.sum_elements(get(i, P), get(i, Q));
+    }
+    return ans;
+}
+
+std::pair<std::vector<int>, const galois_field> get_generating_polynomial(int n, int k, int delta) {
     //Нужно, чтобы n|(q^m - 1)(в нашем случае q == 2, Q = q^m)
     int m;
     int max_m = 10;
@@ -97,17 +154,16 @@ int get_generating_polynomial(int n, int k, int delta) {
         }
     }
     if (!existBCH) {
-        throw "BCH code doesn't exist or order is too big";
+        throw std::overflow_error("BCH code doesn't exist or order is too big");
     }
 
     //многочлен вида x^m + x + 1 = (10...011) неприводим над GF(2)
-    galua_field GF = galua_field(1 << m | 3);
+    auto GF = galois_field(1 << m | 3);
 
     std::set<std::vector<int>> minimal_polynomials;
 
     //если b == 1 - код БЧХ в узком смысле
     int b = 1;
-
     //заполнение множества минимальных многочленов
     //требуется найти минимальные многочлены для элементов alpha^b, alpha^(b+1) ... alpha^(b + delta - 2)
     for (int i = b; i <= b + delta - 2; ++i) {
@@ -120,36 +176,205 @@ int get_generating_polynomial(int n, int k, int delta) {
             }
         }
         for (int j = 1, degree = 2; j <= m_b; ++j, degree *= 2) {
-            std::vector<int> multiplier{1, GF.getElement(i * degree)};
+            std::vector<int> multiplier{GF.getElement(i * degree), 1};
             minimal_polynomial = multiply_polynomial(GF, minimal_polynomial, multiplier);
         }
         minimal_polynomials.insert(minimal_polynomial);
     }
 
     std::vector generating_polynomial{1};
-    for(auto &t: minimal_polynomials) {
+    for (auto &t: minimal_polynomials) {
         generating_polynomial = multiply_polynomial(GF, generating_polynomial, t);
     }
 
-    int result_polynomial = 0;
-    for(int i = 0; i < generating_polynomial.size(); i++) {
-        std::cout << generating_polynomial[i] << ' ';
-        result_polynomial |= (1 << i) * generating_polynomial[generating_polynomial.size() - i - 1];
+    return {generating_polynomial, GF};
+}
+
+std::vector<int> intToBinary(int number) {
+    std::vector<int> binary_representation;
+    while (number > 0) {
+        binary_representation.push_back(number % 2);
+        number /= 2;
     }
 
-    return result_polynomial;
+    return binary_representation;
 }
 
-int code_word(int information_word, int generated_polynomial, int n, int k) {
-    information_word <<= (n - k);
-    std::cout << "\nmoved:\t\t\t\t" << std::bitset<15>(information_word) << '\n';
-    std::cout << "\ngenerated:\t\t\t" << std::bitset<15>(generated_polynomial) << '\n';
-//    information_word % generated_polynomial;
-    std::cout << "moved with mod:\t\t" << std::bitset<15>(generated_polynomial ^ (information_word % generated_polynomial))<< '\n';
-    std::cout << "result:\t\t\t\t" << std::bitset<15>((information_word % generated_polynomial) + information_word)<< '\n';
-
-    return (information_word % generated_polynomial) & information_word;
-
+std::vector<int> shiftLeft(const std::vector<int> &a, size_t shift) {
+    std::vector<int> result(a.size() + shift, 0);
+    for (int i = 0; i < a.size(); ++i) {
+        result[i + shift] = a[i];
+    }
+    return result;
 }
+
+std::vector<int> mod(const galois_field &GF, std::vector<int> a, std::vector<int> &b) {
+    while (a.size() >= b.size()) {
+        std::vector<int> t = shiftLeft(b, a.size() - b.size());
+        a = shrink(summing_polynomial(GF, a, t));
+    }
+
+    return a;
+}
+
+void printVector(std::vector<int> &vector) {
+    for (auto t: vector) {
+        std::cout << t;
+    }
+    std::cout << '\n';
+}
+
+std::vector<int>
+code_word(const galois_field &GF, std::vector<int> &information_word, std::vector<int> &generated_polynomial, int n,
+          int k) {
+    auto shifted = shiftLeft(information_word, n - k);
+    auto m = mod(GF, shifted, generated_polynomial);
+    auto res = summing_polynomial(GF, m, shifted);
+    return res;
+}
+
+int evaluate(const galois_field &GF, std::vector<int> polynomial, int a) {
+    int value = 0;
+    for (int j = 0; j < polynomial.size(); ++j) {
+        value = GF.sum_elements(value, GF.multiply_elements(polynomial[j], GF.getPower(a, j)));
+    }
+    return value;
+}
+
+std::vector<int> get_roots(const galois_field &GF, std::vector<int> &polynomial) {
+    std::vector<int> roots;
+    for (int i = 0; i < (1 << GF.m); ++i) {
+        if (evaluate(GF, polynomial, i) == 0) {
+            roots.push_back(i);
+            if (roots.size() == polynomial.size() - 1) {
+                break;
+            }
+        }
+    }
+    return roots;
+}
+
+std::vector<int> get_syndrome_vector(const galois_field &GF, std::vector<int> &corrupted_word, int delta) {
+    std::vector<int> syndrome(delta - 1);
+    for (int i = 1; i <= 1 + delta - 2; ++i) {
+        syndrome[i - 1] = evaluate(GF, corrupted_word, GF.getElement(i));
+    }
+    return syndrome;
+}
+
+std::vector<int> decoder_berlekamp_massey(const galois_field &GF, std::vector<int> &syndrome, int delta) {
+    std::vector<int> locators{1};
+    std::vector<int> B{1};
+    int r = 1;
+    int m = 0;
+    int L = 0;
+    while (r <= delta - 1) {
+        int delta_r = 0;
+        for (int j = 0; j <= L; ++j) {
+            delta_r = GF.sum_elements(
+                    delta_r,
+                    GF.multiply_elements(locators[j],
+                                         syndrome[r - 1 - j])
+            );
+        }
+        if (delta_r != 0) {
+            std::vector<int> T;
+            std::vector<int> poly{delta_r};
+            poly = shiftLeft(poly, r - m);
+            T = summing_polynomial(GF, locators,
+                                   multiply_polynomial(GF, poly, B));
+            if (2 * L <= r - 1) {
+                B = multiply_polynomial(GF,
+                                        {GF.getInverse(delta_r)},
+                                        locators);
+                locators = T;
+                L = r - L;
+                m = r;
+            } else {
+                locators = T;
+            }
+        }
+        r++;
+    }
+    return locators;
+}
+
+
+//struct bch_code {
+//    galois_field GF;
+//    std::vector<int> generatingPolynomial;
+//    int n;
+//    int k;
+//    int delta;
+//
+//
+//    explicit bch_code(const std::pair<std::vector<int>, galois_field> &pair) : GF(pair.second),
+//                                                                               generatingPolynomial(pair.first) {};
+//
+//    bch_code(int _n, int _k, int _delta) : bch_code(get_generating_polynomial(_n, _k, _delta)) {
+//        n = _n;
+//        k = _k;
+//        delta = _delta;
+//
+//    };
+//
+//    std::vector<int> code_word(std::vector<int> &information_word) {
+//        auto shifted = shiftLeft(information_word, n - k);
+//        auto m = mod(GF, shifted, generatingPolynomial);
+//        auto res = summing_polynomial(GF, m, shifted);
+//        return res;
+//    }
+//
+//    std::vector<int> get_syndrome_vector(const std::vector<int> &corrupted_word) {
+//        std::vector<int> syndrome(delta - 1);
+//        for (int i = 1; i <= 1 + delta - 2; ++i) {
+//            syndrome[i - 1] = evaluate(GF, corrupted_word, GF.getElement(i));
+//        }
+//        return syndrome;
+//    }
+//
+//    std::vector<int> decoder_berlekamp_massey(const std::vector<int> &syndrome) {
+//        std::vector<int> locators{1};
+//        std::vector<int> B{1};
+//        int r = 1;
+//        int m = 0;
+//        int L = 0;
+//        while (r <= delta - 1) {
+//            int delta_r = 0;
+//            for (int j = 0; j <= L; ++j) {
+//                delta_r = GF.sum_elements(
+//                        delta_r,
+//                        GF.multiply_elements(locators[j],
+//                                             syndrome[r - 1 - j])
+//                );
+//            }
+//            if (delta_r != 0) {
+//                std::vector<int> T;
+//                std::vector<int> poly{delta_r};
+//                poly = shiftLeft(poly, r - m);
+//                T = summing_polynomial(GF, locators,
+//                                       multiply_polynomial(GF, poly, B));
+//                if (2 * L <= r - 1) {
+//                    B = multiply_polynomial(GF,
+//                                            {GF.getInverse(delta_r)},
+//                                            locators);
+//                    locators = T;
+//                    L = r - L;
+//                    m = r;
+//                } else {
+//                    locators = T;
+//                }
+//            }
+//            r++;
+//        }
+//        return locators;
+//    }
+//
+//    std::vector<int> find_errors(std::vector<int> &corrupted_word) {
+//        auto locators = decoder_berlekamp_massey(get_syndrome_vector(corrupted_word));
+//
+//    }
+//
+//};
 
 #endif //BCH_CODES_BCH_CODES_H
--- tests.cpp
+++ tests.cpp
@@ -29,7 +29,7 @@
 //(0001) = alpha^15 = (10010) + (10011)
 
 TEST(default_operation, getElement) {
-    galua_field GF(19);
+    galois_field GF(19);
     EXPECT_EQ(14, GF.getElement(11));
     EXPECT_EQ(1, GF.getElement(0));
     EXPECT_EQ(2, GF.getElement(1));
@@ -39,7 +39,7 @@ TEST(default_operation, getElement) {
 }
 
 TEST(default_operation, getLog) {
-    galua_field GF(19);
+    galois_field GF(19);
     EXPECT_EQ(11, GF.getLog(14));
     EXPECT_EQ(0, GF.getLog(1));
     EXPECT_EQ(1, GF.getLog(2));
@@ -49,7 +49,7 @@ TEST(default_operation, getLog) {
 }
 
 TEST(default_operation, sum) {
-    galua_field GF(19);
+    galois_field GF(19);
     EXPECT_EQ(1, GF.sum_elements(0, 1));
     EXPECT_EQ(7, GF.sum_elements(11, 12));
     EXPECT_EQ(1, GF.sum_elements(GF.getElement(8), GF.getElement(2)));
@@ -57,7 +57,7 @@ TEST(default_operation, sum) {
 }
 
 TEST(default_operation, multiply) {
-    galua_field GF(19);
+    galois_field GF(19);
     EXPECT_EQ(3, GF.multiply_elements(3, 1));
     EXPECT_EQ(6, GF.multiply_elements(3, 2));
     EXPECT_EQ(GF.log_to_element[6], GF.multiply_elements(GF.log_to_element[10], GF.log_to_element[11]));
@@ -68,7 +68,7 @@ TEST(default_operation, multiply) {
 }
 
 TEST(default_operation, simple_expression) {
-    galua_field GF(19);
+    galois_field GF(19);
 
     EXPECT_EQ(GF.getElement(6), GF.multiply_elements(
             GF.sum_elements(
@@ -82,8 +82,61 @@ TEST(default_operation, simple_expression) {
     ));
 }
 
+TEST(polynomial_operation, intToBinary) {
+    int x = 19;
+    auto result = intToBinary(x);
+    EXPECT_EQ(result[0], 1);
+    EXPECT_EQ(result[1], 1);
+    EXPECT_EQ(result[2], 0);
+    EXPECT_EQ(result[3], 0);
+    EXPECT_EQ(result[4], 1);
+}
+
+TEST(polynomial_operation, shift) {
+    int x = 19;
+    auto result = shiftLeft(intToBinary(x), 2);
+    EXPECT_EQ(result[0], 0);
+    EXPECT_EQ(result[1], 0);
+    EXPECT_EQ(result[2], 1);
+    EXPECT_EQ(result[3], 1);
+    EXPECT_EQ(result[4], 0);
+    EXPECT_EQ(result[5], 0);
+    EXPECT_EQ(result[6], 1);
+}
+
+TEST(polynomial_operation, shrink) {
+    int x = 19;
+    auto result = intToBinary(x);
+    result.push_back(0);
+    result.push_back(0);
+    result.push_back(0);
+    result.push_back(0);
+    EXPECT_EQ(result[0], 1);
+    EXPECT_EQ(result[1], 1);
+    EXPECT_EQ(result[2], 0);
+    EXPECT_EQ(result[3], 0);
+    EXPECT_EQ(result[4], 1);
+    EXPECT_EQ(result[5], 0);
+    EXPECT_EQ(result[6], 0);
+    EXPECT_EQ(result[7], 0);
+    result = shrink(result);
+    EXPECT_EQ(result.size(), 5);
+    EXPECT_EQ(result[0], 1);
+    EXPECT_EQ(result[1], 1);
+    EXPECT_EQ(result[2], 0);
+    EXPECT_EQ(result[3], 0);
+    EXPECT_EQ(result[4], 1);
+}
+
+
+TEST(polynomial_operation, multiply_polynomial) {
+    auto GF = galois_field(19);
+
+
+}
+
 TEST(generating_polynomial, check_count) {
-    auto GF = galua_field(19);
+    auto GF = galois_field(19);
     EXPECT_EQ(0, GF.sum_elements(GF.getElement(4),
                                  GF.sum_elements(
                                          GF.getElement(2),
@@ -116,39 +169,107 @@ TEST(generating_polynomial, check_count) {
 
 
 TEST(generating_polynomial, check_generating_polynomial) {
-//    auto GF = galua_field(19);
-//    std::vector<int> answer{1, 1, 1, 0, 1, 0, 0, 0, 1};
-    int answer = 1 + 16 + 64 + 128 + 256;
-    int generated = get_generating_polynomial(15, 7, 5);
-    std::cout << std::bitset<15>(generated);
-//    EXPECT_EQ(answer.size(), generated.size());
-//    for (int i = 0; i < answer.size(); ++i) {
-//        EXPECT_EQ(answer[i], generated[i]);
-//    }
-    EXPECT_EQ(answer, generated);
+//    auto GF = galois_field(19);
+    std::vector<int> answer{1, 1, 1, 0, 1, 0, 0, 0, 1};
+    std::reverse(answer.begin(), answer.end());
+//    int answer = 1 + 16 + 64 + 128 + 256;
+    std::vector<int> generated = get_generating_polynomial(15, 7, 5).first;
+//    std::cout << std::bitset<15>(generated);
+
+    EXPECT_EQ(answer.size(), generated.size());
+    for (int i = 0; i < answer.size(); ++i) {
+        EXPECT_EQ(answer[i], generated[i]);
+    }
+//    EXPECT_EQ(answer, generated);
 }
 
 TEST(generating_polynomial, check_generating_polynomial_2) {
-//    auto GF = galua_field(19);
-//    std::vector<int> answer{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    int answer = (1 << 15) - 1;
-    int generated = get_generating_polynomial(15, 7, 9);
-    std::cout << std::bitset<15>(generated);
+//    auto GF = galois_field(19);
+    std::vector<int> answer{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+//    int answer = (1 << 15) - 1;
+    std::vector<int> generated = get_generating_polynomial(15, 7, 9).first;
+//    std::cout << std::bitset<15>(generated);
 
-//    EXPECT_EQ(answer.size(), generated.size());
-//    for (int i = 0; i < answer.size(); ++i) {
-//        EXPECT_EQ(answer[i], generated[i]);
-//    }
-    EXPECT_EQ(answer, generated);
+    EXPECT_EQ(answer.size(), generated.size());
+    for (int i = 0; i < answer.size(); ++i) {
+        EXPECT_EQ(answer[i], generated[i]);
+    }
+//    EXPECT_EQ(answer, generated);
 }
 
 
-TEST(generating_polynomial, coding_word) {
-    int generated = get_generating_polynomial(15, 7, 5);
-//    std::vector<int> information_word{1, 1, 0, 0, 1};
-    int information_word = 25;
-    int coded_word = code_word(information_word, generated, 15, 7);
-    EXPECT_EQ(coded_word, (1 << 12) + (1 << 11) + (1<<8) + (1<<7) + (1<<6) +32 + 16 + 4 + 2);
-
+TEST(coding, coding_word) {
+    auto generated_pair = get_generating_polynomial(15, 7, 5);
+    std::vector<int> generated = generated_pair.first;
+//    std::vector<int> information_word{1, 1, 1, 1, 0, 0, 1};
+    auto information_word = intToBinary(25 + 32 + 64);
+    auto coded_word = code_word(generated_pair.second, information_word, generated, 15, 7);
+    auto expected_result = intToBinary((1 << 14) + (1 << 13) + (1 << 12) +
+                                       (1 << 11) + (1 << 8) + 64 + 32 + 8 + 2);
+//    printVector(expected_result);
+//    printVector(coded_word);
+    EXPECT_EQ(expected_result.size(), coded_word.size());
+    for (int i = 0; i < expected_result.size(); ++i) {
+        EXPECT_EQ(expected_result[i], coded_word[i]);
+    }
 }
 
+TEST(decoding, syndrome_vector) {
+    auto generating_and_field = get_generating_polynomial(15, 7, 5);
+    std::vector<int> generated = generating_and_field.first;
+    galois_field GF = generating_and_field.second;
+    auto information_word = intToBinary(25 + 32 + 64);
+    auto information_word_2 = intToBinary(24 + 32 + 64);
+    auto coded_word = code_word(GF, information_word, generated, 15, 7);
+    auto coded_word_2 = code_word(GF, information_word, generated, 15, 7);
+    std::cout << "Coded word:\n";
+    printVector(coded_word);
+    if (coded_word[0] == 0) {
+        coded_word[0] = 1;
+    } else {
+        coded_word[0] = 0;
+    }
+    std::cout << "Corrupted_word:\n";
+    printVector(coded_word);
+    auto syndrome = get_syndrome_vector(GF, coded_word, 5);
+    std::cout << "Syndrome:\n";
+    for (int i: syndrome) {
+        std::cout << syndrome[i] << ' ';
+//        EXPECT_EQ(i, 0);
+    }
+}
+void corrupt_symbol(std::vector<int>& code, int index) {
+    if (code[index] == 0) {
+        code[index] = 1;
+    } else {
+        code[index] = 0;
+    }
+}
+
+TEST(decoding, decoding_word) {
+    auto generating_and_field = get_generating_polynomial(15, 7, 5);
+    std::vector<int> generated = generating_and_field.first;
+    galois_field GF = generating_and_field.second;
+    auto information_word = intToBinary(25 + 32 + 64);
+    auto coded_word = code_word(GF, information_word, generated, 15, 7);
+
+    corrupt_symbol(coded_word, 6);
+    corrupt_symbol(coded_word, 3);
+
+
+    auto syndrome = get_syndrome_vector(GF, coded_word, 5);
+    auto locators = decoder_berlekamp_massey(GF, syndrome, 5);
+
+    auto roots = get_roots(GF, locators);
+    for (int & root : roots) {
+        root = GF.getLog(GF.getInverse(root));
+    }
+    std::sort(roots.begin(), roots.end());
+    EXPECT_EQ(roots.size(), 2);
+    EXPECT_EQ(3, roots[0]);
+    EXPECT_EQ(6, roots[1]);
+}
+
+//TEST(bch_code, bch_code) {
+//    bch_code bchCode(15, 7, 5);
+//}
