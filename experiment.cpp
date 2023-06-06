1f9537e5342d561ccff86a8566895c5b09ebee16
diS3e <geli1610@yandex.ru>
2023-06-06 05:45:53 +0300

    feat: add graphic of mean list size in Chase list decoding

--- experiment.cpp
+++ experiment.cpp
@@ -15,8 +15,6 @@ bool words_equals(std::vector<int> const &a, std::vector<int> const &b) {
     return true;
 }
 
-
-
 void collect_data(int n, int delta, int precision, int tau) {
     bch_code bchCode(n, delta);
     chase_decoder chaseDecoder(bchCode);
@@ -40,6 +38,28 @@ void collect_data(int n, int delta, int precision, int tau) {
     }
 }
 
+
+void collect_mean_numbers_vector(int n, int delta, int precision, int tau) {
+    bch_code bchCode(n, delta);
+    chase_decoder chaseDecoder(bchCode);
+
+    for (double Eb = 0.0; Eb < 6.0; Eb += 0.5) {
+        double correct = 0;
+        double all = precision;
+        for(int tries = 0; tries < all; tries++) {
+            auto information_word = rnd.get_random_word(bchCode.k);
+            auto coded_word = bchCode.code_word(information_word);
+            std::vector<double> corrupt = chaseDecoder.get_corrupt(coded_word, pow(10.0, (Eb / 10)));
+            auto results = chaseDecoder.chase_decoding_2(corrupt, tau);
+            std::set<std::vector<int>> set(results.begin(), results.end());
+            correct += set.size();
+
+        }
+        std::cout << std::fixed << std::setprecision(6) << Eb << ";" << correct / all << '\n';
+    }
+}
+
+
 int main() {
-    collect_data(63, 11, 1000, 8);
+    collect_mean_numbers_vector(63, 11, 1000, 6);
 }
